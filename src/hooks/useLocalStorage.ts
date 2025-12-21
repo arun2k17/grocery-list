6781a36c6caaa095229e2ebbdd14e4246e8bd8e6
Arunkumar Manickam <armanick@microsoft.com>
2025-12-20 21:23:30 -0800

    static web page

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useState, useEffect, type Dispatch, type SetStateAction } from 'react';
 
 export function useLocalStorage<T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] {
     // Get from local storage or return initial value
